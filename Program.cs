bb1af48aed951e57cbfd0cc48394e21d626e3c62
danil614 <fs159@mail.ru>
2024-05-22 17:13:09 +0500

    feat: added connection with database

--- Program.cs
+++ Program.cs
@@ -1,4 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using spa_project_management;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Получаем строку подключения из файла конфигурации
+var connection = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Добавляем контекст ApplicationContext в качестве сервиса в приложение
+builder.Services.AddDbContext<ApplicationContext>(options => options.UseNpgsql(connection));
 
 // Add services to the container.
 
